8da1cd34ea1ed2e73cd1f7657d6b6d8224f6c7dc
Karolina Joksaite <k.joksaite00@gmail.com>
2022-06-06 14:48:10 +0100

    updated format

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 def fizzbuzz(number) # rubocop:disable Metrics/MethodLength
-fizz = (number % 3).zero?
-buzz = (number % 5).zero?
-   if fizz && buzz
-     'fizzbuzz'
-   elsif fizz
-     'fizz'
-   elsif buzz
-     'buzz'
-   else
-       number
+  fizz = (number % 3).zero?
+  buzz = (number % 5).zero?
+  if fizz && buzz
+    'fizzbuzz'
+  elsif fizz
+    'fizz'
+  elsif buzz
+    'buzz'
+  else
+    number
   end
 end
