695b69578028166c4163b65a90e246ded971fe09
Karolina Joksaite <k.joksaite00@gmail.com>
2022-06-06 14:26:32 +0100

    finished tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz(number)
-  if number % 3 == 0 then return "fizz" end
-end 
+def fizzbuzz(number) # rubocop:disable Metrics/MethodLength
+   if (number % 3).zero? && (number % 5).zero?
+     'fizzbuzz'
+   elsif (number % 3).zero?
+     'fizz'
+   elsif (number % 5).zero?
+     'buzz'
+   else
+     number
+   end
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,20 +3,25 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  context 'given a multiple of 5' do
-    it 'returns buzz' do
-      expect(fizzbuzz(5)).to eq 'buzz'
-    end
+  it 'returns buzz if multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(10)).to eq 'buzz'
+    expect(fizzbuzz(20)).to eq 'buzz'
   end
-  context 'given a multiple of 3' do
-    it 'returns fizz' do
-      expect(fizzbuzz(3)).to eq 'fizz'
-    end
+  it 'returns fizz if multiple of 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(12)).to eq 'fizz'
+    expect(fizzbuzz(18)).to eq 'fizz'
   end
-  context 'given a multiple of 3 and 5' do
-    it 'returns fizzbuzz' do
-      expect(fizzbuzz(15)).to eq 'fizzbuzz'
-    end
+  it 'returns fizzbuzz if multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+    expect(fizzbuzz(60)).to eq 'fizzbuzz'
+  end
+  it 'returns number if not a multiple of 3 or 5' do
+    expect(fizzbuzz(1)).to eq 1
+    expect(fizzbuzz(4)).to eq 4
+    expect(fizzbuzz(31)).to eq 31
   end
 end
 
